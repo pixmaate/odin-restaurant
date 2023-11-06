2978b9d6994bb673f90bd70457578f6eddb29637
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-11-06 22:16:21 +0100

    main objects set up for mainpage

--- src/homepage.js
+++ src/homepage.js
@@ -4,9 +4,14 @@ export {createHomePage};
 function createHomePage() {
     const mainContentArea = document.querySelector('#content');
 
+    const testElement = document.createElement('p');
+    testElement.textContent = 'TEST'
+
 
     function drawPage() {
         const headerElement = createHeader().addHeader(mainContentArea);
+        const mainContentElement = createMainContent().addContent(headerElement);
+        
     };
 
     
@@ -28,14 +33,85 @@ function createHeader() {
         titleText.textContent = 'Shrimp Bar';
         headerArea.appendChild(titleText);
 
+        //headermenu
+        const buttonWrapper = document.createElement('div');
+        buttonWrapper.classList.add('buttonWrapper');
+        const homeButton = document.createElement('button');
+        homeButton.textContent = 'Home'
+        const foodsButton = document.createElement('button');
+        foodsButton.textContent = 'Our Menu'
+        const aboutButton = document.createElement('button');
+        aboutButton.textContent = 'About Us'
+
+        buttonWrapper.appendChild(homeButton);
+        buttonWrapper.appendChild(foodsButton);
+        buttonWrapper.appendChild(aboutButton);
+
+        headerArea.appendChild(buttonWrapper);
+
 
 
         //append header to Content
         element.appendChild(headerArea);
+        return(buttonWrapper);
 
 
     };
 
     return {addHeader}
 
+};
+
+function createMainContent() {
+
+    function addContent(element) {
+        const mainContentWrapper = document.createElement('div');
+
+
+        // left content area
+        const mainContentBoxLeft = document.createElement('div');
+        const mainContentImageDiv = document.createElement('img');
+        const mainContentImage = document.createElement('img');
+        mainContentImageDiv.appendChild(mainContentImage);
+
+
+        
+        mainContentWrapper.appendChild(mainContentBoxLeft);
+
+        // right content area
+        const mainContentBoxRight = document.createElement('div');
+
+
+        mainContentWrapper.appendChild(mainContentBoxRight);
+
+
+
+
+        element.after(mainContentWrapper);
+
+
+    }
+
+
+    return {addContent}
+};
+
+function createInfoArea() {
+
+    function addInfo(element) {
+
+    };
+
+
+    return{addInfo}
+};
+
+function createFooter() {
+
+    function makeFooter(element) {
+
+    };
+
+
+    return {makeFooter}
 };
