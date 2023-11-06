65ba5abcd8cc06cb0c6e86bf9be9ddd0ace6da08
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-11-06 18:52:21 +0100

    set up homepage object

--- src/homepage.js
+++ src/homepage.js
@@ -4,7 +4,26 @@ export {createHomePage};
 function createHomePage() {
 
     function drawPage() {
-        alert('hi')
+        addHeader();
+    };
+
+    function addHeader() {
+        const mainContentArea = document.querySelector('#content');
+        const headerArea = document.createElement('div');
+
+        headerArea.classList.add('headerArea');
+
+        //title Text
+        const titleText = document.createElement('h1');
+        titleText.textContent = 'Shrimp Bar';
+        headerArea.appendChild(titleText);
+
+
+
+        //append header to Content
+        mainContentArea.appendChild(headerArea);
+
+
     };
 
 
